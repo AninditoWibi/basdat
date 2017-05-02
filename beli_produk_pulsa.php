eec1efd6cb4ab8e67b6ff613596ca46629cc2ef7
Muhammad Fadhillah <Muhammad Fadhillah>
2017-05-02 21:09:10 +0700

    Menampilkan daftar produk

---
+++ beli_produk_pulsa.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Beli Produk</title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.2/css/materialize.min.css">
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    </head>
+    <body>
+        <div class="container">
+            <div class="center-align">
+                <h2 class="teal-text">Daftar Produk Pulsa</h2>
+            </div>
+            <table class="highlight">
+                <thead>
+                    <tr>
+                        <th>Kode Produk</th>
+                        <th>Nama Produk</th>
+                        <th>Harga</th>
+                        <th class="center-align">Deskripsi</th>
+                        <th>Nominal</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>P0000001</td>
+                        <td>Pulsa Telkomsel</td>
+                        <td>12000</td>
+                        <td>Untuk nelpon dan internetan</td>
+                        <td>10000</td>
+                        <td>
+                            <button class="btn waves-effect waves-light" type="submit" name="beli">Beli
+                                <i class="material-icons right">shopping_cart</i>
+                            </button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>P0000021</td>
+                        <td>Pulsa XL</td>
+                        <td>12000</td>
+                        <td>Untuk nelpon dan internetan</td>
+                        <td>10000</td>
+                        <td>
+                            <button class="btn waves-effect waves-light" type="submit" name="beli">Beli
+                                <i class="material-icons right">shopping_cart</i>
+                            </button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>P0000081</td>
+                        <td>Pulsa Smartfren</td>
+                        <td>12000</td>
+                        <td>Untuk nelpon dan internetan</td>
+                        <td>10000</td>
+                        <td>
+                            <button class="btn waves-effect waves-light" type="submit" name="beli">Beli
+                                <i class="material-icons right">shopping_cart</i>
+                            </button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>P0000198</td>
+                        <td>Paket Airtel</td>
+                        <td>230000</td>
+                        <td>Untuk nelpon dan internetan</td>
+                        <td>200000</td>
+                        <td>
+                            <button class="btn waves-effect waves-light" type="submit" name="beli">Beli
+                                <i class="material-icons right">shopping_cart</i>
+                            </button>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.2/js/materialize.min.js"></script>
+    </body>
+</html>
---
+++ beli_shipped_produk.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Beli Produk</title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.2/css/materialize.min.css">
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    </head>
+    <body>
+        <div class="container">
+            <div class="center-align">
+                <h2 class="teal-text">Daftar Produk</h2>
+            </div>
+            <table class="highlight">
+                <thead>
+                    <tr>
+                        <th>Kode Produk</th>
+                        <th>Nama Produk</th>
+                        <th>Harga</th>
+                        <th class="center-align">Deskripsi</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>S0000001</td>
+                        <td>Maalox</td>
+                        <td>59900</td>
+                        <td>Other repair of joint</td>
+                        <td>
+                            <button class="btn waves-effect waves-light" type="submit" name="beli">Beli
+                                <i class="material-icons right">shopping_cart</i>
+                            </button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>S0000071</td>
+                        <td>Lipitor</td>
+                        <td>789700</td>
+                        <td>Tracheoscopy thru stoma</td>
+                        <td>
+                            <button class="btn waves-effect waves-light" type="submit" name="beli">Beli
+                                <i class="material-icons right">shopping_cart</i>
+                            </button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>S0000175</td>
+                        <td>Piroxicam</td>
+                        <td>99789900</td>
+                        <td>Remov intralum nose FB</td>
+                        <td>
+                            <button class="btn waves-effect waves-light" type="submit" name="beli">Beli
+                                <i class="material-icons right">shopping_cart</i>
+                            </button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>S0000202</td>
+                        <td>Ranitidine Hydrochloride</td>
+                        <td>889801</td>
+                        <td>Abdominal wall sinogram</td>
+                        <td>
+                            <button class="btn waves-effect waves-light" type="submit" name="beli">Beli
+                                <i class="material-icons right">shopping_cart</i>
+                            </button>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.2/js/materialize.min.js"></script>
+    </body>
+</html>

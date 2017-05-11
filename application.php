ca7c1f46d177699f9f402cee492bb3def9ecad2f
Muhammad Fadhillah <Muhammad Fadhillah>
2017-05-11 20:17:24 +0700

    Tambahin dokumentasi cara pakai dan contoh

--- application.php
+++ application.php
@@ -2,6 +2,25 @@
    session_start();
    set_search_path();
 
+   /*
+        Cara pakai:
+        1. Semua perintah disini harus melewati method POST
+
+        2. Cara pass value bisa menggunakan tag <input> dengan type none.
+           Contoh: <input type="none" name="password" value="12345"></input>
+
+        3. Cara menerima seperti biasa, $_POST['password'].
+           Maka $_POST['password'] akan menyimpan nilai 12345
+
+        4. Ingat! semua tag <input> harus didalam tag <form> dan hanya bisa dikirim
+           dengan tag <input> atau <button> dengan type=submit !!!
+
+        Contoh lengkap:
+        <form method="post">
+        	<input type="none" name="harga_barang" value="100000">
+        	<button type="submit" name="button">Beli</button>
+        </form>
+   */
    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
       if($_POST['command'] === 'login') {
           login($_POST['username'], $_POST['password']);
@@ -20,6 +39,9 @@
       }
    }
 
+   /*
+        Berfungsi untuk mengecek koneksi ke database dan dapat digunakan untuk eksekusi query.
+   */
    function connectDB() {
       $servername = "localhost";
       $username = "muhammad.fadhillah";
@@ -53,6 +75,9 @@
 
    }
 
+   /*
+        Menerima input berupa query, dan mengembalikan resource yang bisa di fetch, atau FALSE jika query gagal.
+   */
    function execute_query($query)
    {
        $conn = connectDB();

a6d4f5d56126144fe322d01f65193a27d08cc5ff
rizfarchanresthu <resthuraito@gmail.com>
2017-05-11 19:21:55 +0700

    Add files via upload
    benerin login

--- login.php
+++ login.php
@@ -9,44 +9,31 @@
 </head>
 <body>
     <?php
-    include "navbar.php"
+    @session_start();
+    include "navbar.php";
+    $msg = "";
+    if(isset($_SESSION['wrong'])){
+      $msg = "Email atau Password salah";
+      echo "<div>".$msg."</div>";
+      session_unset('wrong');
+    }
     ?>
-	<div class="container">
-		<div class="row">
-			<div class="card-panel z-depth-2 col s10 push-s1">
-				<div class="center-align">
-					<h2 class="teal-text center-align">Log In</h2>
-				</div>
-				<div class="container">
-					<div class="row">
-						<form class="col s12">
-							<div class="row">
-								<div class="input-field col s12">
-									<input type="email" id="email" name="email" placeholder="E-mail" autofocus required class="validate">
-									<label for="email" data-error="wrong" data-success="right">E-mail</label>
-								</div>
-							</div>
-							<div class="row">
-								<div class="input-field col s12">
-									<input type="password" id="password" name="password" placeholder="Password" autofocus required class="validate" pattern=".{6,}">
-									<label for="password" >Password</label>
-								</div>
-							</div>
-							<div class="row">
-								<div class="input-field col s12 center-align">
-									<button class="btn waves-effect waves-light btn-large" type="submit" name="action">Submit
-                                        <i class="material-icons right">send</i>
-                                    </button>
-								</div>
-							</div>
-						</form>
-					</div>
-				</div>
+    <h2 class="teal-text center-align">Log In</h2>
+	<div class="row" style="width: 500px">
+		<form class="col s12" action="loginprocess.php" method="POST">
+			<div class="input-field col s12">
+				<input type="email" id="email" name="email" placeholder="E-mail" autofocus required class="validate">
+       <label for="email" data-error="wrong" data-success="right">E-mail</label>
 			</div>
-		</div>
+			<div class="input-field col s12">
+				<input type="password" id="password" name="password" placeholder="Password" autofocus required class="validate" pattern=".{6,}">
+        <label for="password" >Password</label>
+			</div>
+			<input class="btn waves-effect waves-light" type="submit" value="Submit">
+		</form>
 	</div>
-
-
+    
+    
     <!--Import jQuery before materialize.js-->
     <script type="text/javascript" src="jquery-3.1.0.min.js"></script>
     <script type="text/javascript" src="web/src/js/materialize.min.js"></script>
